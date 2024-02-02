05dfb4e7e0ffdf976c079a725d43dff757d685ea
jmboon <justinmichaelboon@gmail.com>
2024-02-02 01:50:51 +0000

    cleaned provider

--- constants/environment.ts
+++ constants/environment.ts
@@ -2,53 +2,30 @@ import { ethers } from 'ethers'
 import { config as dotenvConfig } from 'dotenv'
 import { abi as IflashMulti } from '../artifacts/contracts/v2/flashMultiTest.sol/flashMultiTest.json'
 import { abi as IflashDirect } from '../artifacts/contracts/v2/flashDirectTest.sol/flashDirectTest.json'
-import { provider as p, checkProvider } from './provider'
+import { provider, wallet, signer } from './provider'
+import { logger } from './logger'
 export const dotenv = dotenvConfig({
     path: `.env.${process.env.NODE_ENV == 'test' ? 'test' : 'live'}`,
 })
 
-export class Environment {
-    constructor() {}
-
-    getWallet(): {
-        wallet: ethers.Wallet
-        signer: ethers.Signer
-    } {
-        if (process.env.PRIVATE_KEY === undefined) {
-            throw new Error('No private key set in .env file')
-            console.log(process.env.PRIVATE_KEY)
-        }
-        let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-        let signer = wallet.connect(provider)
-        return { wallet, signer }
-    }
-
-    getContracts(): {
-        flashMulti: ethers.Contract
-        flashDirect: ethers.Contract
-    } {
-        if (process.env.FLASH_MULTI && process.env.FLASH_DIRECT === undefined) {
-            throw new Error('No flashMultiID set in .env file')
-        }
-
-        const flashMultiID = process.env.FLASH_MULTI
-        const flashDirectID = process.env.FLASH_DIRECT
-
-        if (flashMultiID === undefined || flashDirectID === undefined) {
-            throw new Error('No contract address set in .env file')
-        }
-        let flashMulti: ethers.Contract
-        let flashDirect: ethers.Contract
-        flashMulti = new ethers.Contract(flashMultiID, IflashMulti, signer)
-        flashDirect = new ethers.Contract(flashDirectID, IflashDirect, signer)
-        return { flashMulti, flashDirect }
-    }
+if (
+    process.env.FLASH_MULTI === undefined ||
+    process.env.FLASH_DIRECT === undefined
+) {
+    logger.error('No contract address set in .env file')
+    throw new Error('No contract address set in .env file')
 }
 
-export const env = new Environment()
-export const wallet = env.getWallet().wallet
-export const signer = env.getWallet().signer
-export const walletAddress = wallet.address
-export const provider = p
-export const flashMulti = env.getContracts().flashMulti
-export const flashDirect = env.getContracts().flashDirect
+export const flashMultiID = process.env.FLASH_MULTI
+export const flashDirectID = process.env.FLASH_DIRECT
+
+export const flashMulti = new ethers.Contract(
+    flashMultiID,
+    IflashMulti,
+    provider
+)
+export const flashDirect = new ethers.Contract(
+    flashDirectID,
+    IflashDirect,
+    provider
+)
---
+++ scripts/deployTiny.ts
@@ -0,0 +1,24 @@
+import { ethers } from 'hardhat'
+import { config as dotenvConfig } from 'dotenv'
+// import { signer } from '../constants/environment'
+import { provider, wallet, signer } from '../constants/provider'
+dotenvConfig({ path: `.env.${process.env.NODE_ENV}` })
+import {
+    abi as ITiny,
+    bytecode,
+} from '../artifacts/contracts/v2/tiny.sol/Tiny.json'
+
+// import { wallet } from '../constants/environment'
+
+const TinyFactory = new ethers.ContractFactory(ITiny, bytecode, wallet)
+async function main() {
+    const tiny = await TinyFactory.deploy()
+    console.log('Contract deployed to:', await tiny.getAddress())
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
