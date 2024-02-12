02bba90663254f0716546c9430cc93e23b7884d6
Hank Honeymoon <justinmichaelboon@gmail.com>
2024-02-12 23:13:53 +0100

    profittokenWmatic pairs

--- scripts/v2/modules/gasTokenPricesInWmatic.ts
+++ scripts/v2/modules/gasTokenPricesInWmatic.ts
@@ -3,6 +3,7 @@ import { abi as IUniswapv2Router02 } from "@uniswap/v2-periphery/build/IUniswapV
 import { abi as IUniswapV2Factory } from "@uniswap/v2-core/build/IUniswapV2Factory.json";
 import { abi as IERC20 } from "@uniswap/v2-core/build/IERC20.json";
 import { getAmountsOut } from "./getAmountsIOJS";
+import { GasPool } from "../../../constants/interfaces";
 import { fu, pu } from "../../modules/convertBN";
 import { provider } from "../../../constants/provider";
 import { ethers } from "ethers";
@@ -16,45 +17,60 @@ import {
 import { wmatic, zero } from "../../../constants/environment";
 import { string } from "hardhat/internal/core/params/argumentTypes";
 
-// const gasTokenAddresses = gasTokens.map((gt) => gt.address);
+let gasPools: GasPool[] = [];
 
-export const gasTokenPricesInWmatic = async () => {
-    const gasTokenWmaticPools: string[] = [];
+gasTokenPricesInWmatic().then((res) => {
+    filterPools(gasPools);
+});
+
+export async function gasTokenPricesInWmatic(): Promise<GasPool[] | undefined> {
+    let gasPool: GasPool;
     const wmaticID = await wmatic.getAddress();
     let gasWmaticRoute;
-    const bigFactory = new ethers.Contract(uniswapV2Factory.QUICK, IUniswapV2Factory, provider);
-    for (let tokenID in gasTokens) {
-        if (tokenID !== wmaticID) {
-            gasWmaticRoute = await bigFactory.getPair(tokenID, wmaticID);
-            gasTokenWmaticPools.push(gasWmaticRoute);
+    for (let key in uniswapV2Factory) {
+        const factoryID = uniswapV2Factory[key];
+        const bigFactory = new ethers.Contract(uniswapV2Factory[key], IUniswapV2Factory, provider);
+        for (let key in gasTokens) {
+            const tokenID = gasTokens[key];
+            if (tokenID !== wmaticID) {
+                gasWmaticRoute = await bigFactory.getPair(wmaticID, tokenID);
+                if (gasWmaticRoute !== zero) {
+                    const gasWmaticPair = new ethers.Contract(gasWmaticRoute, IPair, provider);
+                    const gasWmaticLiquidity = await gasWmaticPair.totalSupply();
+                    gasPool = {
+                        ticker: key + "WMATIC",
+                        address: tokenID,
+                        liquidity: gasWmaticLiquidity,
+                    };
+                    // console.log("getTokenPricesInWmatic: ", gasPool);
+                    gasPools.push(gasPool);
+                }
+            }
         }
     }
-    console.log(gasTokenWmaticPools);
-};
+    // gasPools = await filterPools(gasPools);
+    // console.log(gasPools);
+    return gasPools;
+}
 
-//     wmaticPrices.push(gasTokens)
+// gasTokenPricesInWmatic();
+// // WRITE FUNCTION TO FIND GASPOOL ENTRIES OF THE SAME KEY AND FILTER BY LIQUIDITY
+async function filterPools(gasPools: GasPool[]) {
+    let filteredPools: GasPool[] = [];
 
-//     const gasTokenPrices = await Promise.all(
-//         gasTokens.map(async (gt: string) => {
-//             const gtContract = new ethers.Contract(
-//                 gt,
-//                 IERC20,
-//                 provider
-//             );
-//             const gtDecimals = await gtContract.decimals();
-//             const gtPrice = await getAmountsOut(
-//                 pu(1, gtDecimals),
-//                 [uniswapV2Router, uniswapV2Factory],
-//                 [gt.address, "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"]
-//             );
-//             return {
-//                 name: gt.name,
-//                 address: gt.address,
-//                 price: fu(gtPrice, wmaticPrice),
-//             };
-//         })
-//     );
+    for (let newPool of gasPools) {
+        let extantPool = filteredPools.find((p) => p.ticker === newPool.ticker);
 
-//     return gasTokenPrices;
-// };
-// ```
+        if (!extantPool) {
+            // If there's no pool with the same ticker in the array, add the current pool
+            filteredPools.push(newPool);
+        } else if (BigInt(newPool.liquidity) > BigInt(extantPool.liquidity)) {
+            // If there's a pool with the same ticker and lower liquidity, replace it with the current pool
+            filteredPools = filteredPools.filter((p) => p.ticker !== newPool.ticker);
+            filteredPools.push(newPool);
+        }
+    }
+
+    console.log("filteredPoolsFunction: ", filteredPools);
+    return filteredPools;
+}
