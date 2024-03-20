997fc65eb9ad8e8c48975ed08e1309077b78582d
Hank Honeymoon <justinmichaelboon@gmail.com>
2024-03-20 23:45:50 +0000

    live deployed confirmed profitable txs
    Former-commit-id: bb3a78bfdd9c77648f0a45e8aa2df989b4136e7b

--- scripts/v2/modules/transaction/notify.ts
+++ scripts/v2/modules/transaction/notify.ts
@@ -1,7 +1,9 @@
 import { logger } from "../../../../constants/logger";
 import { BoolTrade, Profit } from "../../../../constants/interfaces";
 import { tradeLogs } from "../tradeLog";
-import TelegramBot from "node-telegram-bot-api";
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
@@ -11,15 +13,27 @@ const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 export async function telegramInfo(message: string): Promise<void> {
     try {
         if (!TELEGRAM_BOT_TOKEN) {
-            throw new Error("Telegram bot token not found in environment variables");
+            throw new Error(
+                "Telegram bot token not found in environment variables",
+            );
         }
         if (!TELEGRAM_CHAT_ID) {
-            throw new Error("Telegram chat id not found in environment variables");
+            throw new Error(
+                "Telegram chat id not found in environment variables",
+            );
         } else {
-            const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
-            const note = await bot.sendMessage(TELEGRAM_CHAT_ID, message);
-            const sent = note.message_id;
-            console.log("Telegram message sent: " + sent);
+            const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
+            bot.telegram
+                .sendMessage(TELEGRAM_CHAT_ID, message)
+                .then((sentMessage) => {
+                    console.log(
+                        "Telegram message sent: " + sentMessage.message_id,
+                    );
+                })
+                .catch((error) => {
+                    logger.error("TELEGRAM ERROR [telegramInfo()]: " + error);
+                });
         }
     } catch (error: any) {
         logger.error("TELEGRAM ERROR [telegramInfo()]: " + error);
@@ -31,17 +45,26 @@ export async function notify(trade: BoolTrade) {
     let logs = await tradeLogs(trade);
     const message = `Trade ${trade.ticker} on ${
         (trade.loanPool.exchange, trade.target.exchange)
-    } is ongoing. Projected profit: ${trade.profits.WMATICProfit} MATIC equivalent. \n\n`;
-    ` ${JSON.stringify(logs.data)}`;
+    } is ongoing. Projected profit: ${
+        trade.profits.WMATICProfit
+    } MATIC equivalent. \n\n`;
+    ` ${JSON.stringify(logs.tinyData)}`;
     try {
         if (!TELEGRAM_BOT_TOKEN) {
-            throw new Error("Telegram bot token not found in environment variables");
+            throw new Error(
+                "Telegram bot token not found in environment variables",
+            );
         }
         if (!TELEGRAM_CHAT_ID) {
-            throw new Error("Telegram chat id not found in environment variables");
+            throw new Error(
+                "Telegram chat id not found in environment variables",
+            );
         } else {
-            const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
-            const note = await bot.sendMessage(TELEGRAM_CHAT_ID, message);
+            const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+            const note = await bot.telegram.sendMessage(
+                TELEGRAM_CHAT_ID,
+                message,
+            );
             const sent = note.message_id;
             console.log("Telegram message sent: " + sent);
         }
---
+++ test/testTGBot.ts
@@ -0,0 +1,31 @@
+import TelegramBot from "node-telegram-bot-api";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+export async function testTGBot() {
+    const message = "BOT WORKING";
+    try {
+        if (!TELEGRAM_BOT_TOKEN) {
+            throw new Error(
+                "Telegram bot token not found in environment variables",
+            );
+        }
+        if (!TELEGRAM_CHAT_ID) {
+            throw new Error(
+                "Telegram chat id not found in environment variables",
+            );
+        } else {
+            console.log("Bot token: ", TELEGRAM_BOT_TOKEN);
+            console.log("Chat ID: ", TELEGRAM_CHAT_ID);
+            const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
+            const note = await bot.sendMessage(TELEGRAM_CHAT_ID, message);
+        }
+    } catch (error: any) {
+        console.log("TELEGRAM ERROR [telegramInfo()]: " + error);
+        return;
+    }
+}
+testTGBot();
