17b7d11e64515b48560249907c0a335604afa3e4
Justin Boon <justinmichaelboon@gmail.com>
2024-05-07 14:56:44 +0200

    gitignore hh.log

--- scripts/v2/classes/Prices.ts
+++ scripts/v2/classes/Prices.ts
@@ -6,19 +6,19 @@ import { Pair, ReservesData } from "../../../constants/interfaces";
  */
 
 export class Prices {
-    poolID: string;
-    priceInBN: BN;
-    priceOutBN: BN;
-    reserves: ReservesData;
-    constructor(poolID: string, reserves: ReservesData) {
-        if (!reserves) {
-            throw new Error("Reserves is undefined");
-        }
-        this.poolID = poolID;
-        this.reserves = reserves;
-        // reserves.reserveIn = reserves.reserveIn;
-        // reserves.reserveOut = reserves.reserveOut;
-        this.priceInBN = reserves.reserveInBN.div(reserves.reserveOutBN);
-        this.priceOutBN = reserves.reserveOutBN.div(reserves.reserveInBN);
-    }
+	poolID: string;
+	priceInBN: BN;
+	priceOutBN: BN;
+	reserves: ReservesData;
+	constructor(poolID: string, reserves: ReservesData) {
+		if (!reserves) {
+			throw new Error("Reserves is undefined");
+		}
+		this.poolID = poolID;
+		this.reserves = reserves;
+		// reserves.reserveIn = reserves.reserveIn;
+		// reserves.reserveOut = reserves.reserveOut;
+		this.priceInBN = reserves.reserveInBN.div(reserves.reserveOutBN);
+		this.priceOutBN = reserves.reserveOutBN.div(reserves.reserveInBN);
+	}
 }
