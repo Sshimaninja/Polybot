22c0183f9573873de131d2de14511e45637fc5a2
Justin Boon <justinmichaelboon@gmail.com>
2024-06-06 15:22:21 +0200

    updated readme for openSourcing
    Former-commit-id: 2accc4c93e9170835459330395ea7074990e6a92

--- scripts/v2/modules/transaction/params.ts
+++ scripts/v2/modules/transaction/params.ts
@@ -9,112 +9,112 @@ import { logger } from "../../../../constants/logger";
 import { approve } from "./approve";
 
 export interface PSwap {
-    routerAID: string;
-    routerBID: string;
-    tradeSize: bigint;
-    amountOutA: bigint;
-    path0: string[];
-    path1: string[];
-    to: string;
-    deadline: number;
+	routerAID: string;
+	routerBID: string;
+	tradeSize: bigint;
+	amountOutA: bigint;
+	path0: string[];
+	path1: string[];
+	to: string;
+	deadline: number;
 }
 
 export interface PFlash {
-    loanFactory: string;
-    loanRouter: string;
-    targetRouter: string;
-    tokenInID: string;
-    tokenOutID: string;
-    amountIn: bigint;
-    amountOut: bigint;
-    amountToRepay: bigint;
-    deadline: number;
+	loanFactory: string;
+	loanRouter: string;
+	targetRouter: string;
+	tokenInID: string;
+	tokenOutID: string;
+	amountIn: bigint;
+	amountOut: bigint;
+	amountToRepay: bigint;
+	deadline: number;
 }
 
 export async function params(trade: BoolTrade): Promise<{
-    swap: Promise<ethers.ContractTransaction>;
-    swapParams: PSwap;
-    flashParams: PFlash;
+	swap: Promise<ethers.ContractTransaction>;
+	swapParams: PSwap;
+	flashParams: PFlash;
 }> {
-    const d = Math.floor(Date.now() / 1000) + 60 * 1; // 1 minute
-    let p: PSwap = {
-        routerAID: await trade.target.router.getAddress(), //high Output tokenIn to tokenOut
-        routerBID: await trade.loanPool.router.getAddress(), //high Output tokenOut to tokenIn
-        tradeSize: trade.tradeSizes.loanPool.tradeSizeTokenIn.size,
-        amountOutA: trade.quotes.target.tokenOutOut, //high Output tokenIn to tokenOut
-        path0: [trade.tokenIn.data.id, trade.tokenOut.data.id],
-        path1: [trade.tokenOut.data.id, trade.tokenIn.data.id],
-        to: await signer.getAddress(),
-        deadline: d,
-    };
-    let pf: PFlash = {
-        loanFactory: await trade.loanPool.factory.getAddress(),
-        loanRouter: await trade.loanPool.router.getAddress(),
-        targetRouter: await trade.target.router.getAddress(),
-        tokenInID: trade.tokenIn.data.id,
-        tokenOutID: trade.tokenOut.data.id,
-        amountIn: trade.tradeSizes.loanPool.tradeSizeTokenIn.size,
-        amountOut: trade.quotes.target.tokenOutOut,
-        amountToRepay: trade.loanPool.amountRepay,
-        deadline: d,
-    };
-    let swap: Promise<ethers.ContractTransaction> =
-        {} as Promise<ethers.ContractTransaction>;
-    if (trade.type == "single") {
-        swap = trade.contract.swapSingle.populateTransaction(
-            p.routerAID,
-            p.routerBID,
-            p.tradeSize,
-            p.amountOutA,
-            p.path0,
-            p.path1,
-            p.to,
-            p.deadline,
-            {
-                Type: 2,
-                gasLimit: trade.gas.gasEstimate,
-                maxFeePerGas: trade.gas.maxFee,
-                maxPriorityFeePerGas: trade.gas.maxPriorityFee,
-            },
-        );
-    }
-    if (trade.type == "multi") {
-        swap = trade.contract.swapMulti.populateTransaction(
-            p.routerAID,
-            p.routerBID,
-            p.tradeSize,
-            p.amountOutA,
-            p.path0,
-            p.path1,
-            p.to,
-            p.deadline,
-            {
-                Type: 2,
-                gasLimit: trade.gas.gasEstimate,
-                maxFeePerGas: trade.gas.maxFee,
-                maxPriorityFeePerGas: trade.gas.maxPriorityFee,
-            },
-        );
-    }
-    if (trade.type.includes("flash")) {
-        swap = trade.contract.flashSwap.populateTransaction(
-            pf.loanFactory,
-            pf.loanRouter,
-            pf.targetRouter,
-            pf.tokenInID,
-            pf.tokenOutID,
-            pf.amountIn,
-            pf.amountOut,
-            pf.amountToRepay,
-            {
-                Type: 2,
-                gasLimit: trade.gas.gasEstimate,
-                maxFeePerGas: trade.gas.maxFee,
-                maxPriorityFeePerGas: trade.gas.maxPriorityFee,
-            },
-        );
-    }
-    return { swap, swapParams: p, flashParams: pf };
+	const d = Math.floor(Date.now() / 1000) + 60 * 1; // 1 minute
+	let p: PSwap = {
+		routerAID: await trade.target.router.getAddress(), //high Output tokenIn to tokenOut
+		routerBID: await trade.loanPool.router.getAddress(), //high Output tokenOut to tokenIn
+		tradeSize: trade.tradeSizes.loanPool.tradeSizeTokenIn.size,
+		amountOutA: trade.quotes.target.tokenOutOut, //high Output tokenIn to tokenOut
+		path0: [trade.tokenIn.data.id, trade.tokenOut.data.id],
+		path1: [trade.tokenOut.data.id, trade.tokenIn.data.id],
+		to: await signer.getAddress(),
+		deadline: d,
+	};
+	let pf: PFlash = {
+		loanFactory: await trade.loanPool.factory.getAddress(),
+		loanRouter: await trade.loanPool.router.getAddress(),
+		targetRouter: await trade.target.router.getAddress(),
+		tokenInID: trade.tokenIn.data.id,
+		tokenOutID: trade.tokenOut.data.id,
+		amountIn: trade.tradeSizes.loanPool.tradeSizeTokenIn.size,
+		amountOut: trade.quotes.target.tokenOutOut,
+		amountToRepay: trade.loanPool.amountRepay,
+		deadline: d,
+	};
+	let swap: Promise<ethers.ContractTransaction> =
+		{} as Promise<ethers.ContractTransaction>;
+	if (trade.type == "single") {
+		swap = trade.contract.swapSingle.populateTransaction(
+			p.routerAID,
+			p.routerBID,
+			p.tradeSize,
+			p.amountOutA,
+			p.path0,
+			p.path1,
+			p.to,
+			p.deadline,
+			{
+				Type: 2,
+				gasLimit: trade.gas.gasEstimate,
+				maxFeePerGas: trade.gas.maxFee,
+				maxPriorityFeePerGas: trade.gas.maxPriorityFee,
+			},
+		);
+	}
+	if (trade.type == "multi") {
+		swap = trade.contract.swapMulti.populateTransaction(
+			p.routerAID,
+			p.routerBID,
+			p.tradeSize,
+			p.amountOutA,
+			p.path0,
+			p.path1,
+			p.to,
+			p.deadline,
+			{
+				Type: 2,
+				gasLimit: trade.gas.gasEstimate,
+				maxFeePerGas: trade.gas.maxFee,
+				maxPriorityFeePerGas: trade.gas.maxPriorityFee,
+			},
+		);
+	}
+	if (trade.type.includes("flash")) {
+		swap = trade.contract.flashSwap.populateTransaction(
+			pf.loanFactory,
+			pf.loanRouter,
+			pf.targetRouter,
+			pf.tokenInID,
+			pf.tokenOutID,
+			pf.amountIn,
+			pf.amountOut,
+			pf.amountToRepay,
+			{
+				Type: 2,
+				gasLimit: trade.gas.gasEstimate,
+				maxFeePerGas: trade.gas.maxFee,
+				maxPriorityFeePerGas: trade.gas.maxPriorityFee,
+			},
+		);
+	}
+	return { swap, swapParams: p, flashParams: pf };
 }
 // function flashSwap(
 //     address loanFactory,
--- scripts/v2/populateTrade.ts
+++ scripts/v2/populateTrade.ts
@@ -12,116 +12,116 @@ import { logger } from "../../constants/logger";
 import { params } from "./modules/transaction/params";
 
 export async function populateTrade(trade: BoolTrade): Promise<BoolTrade> {
-    const calc = new AmountConverter(trade);
-    trade.tradeSizes = await calc.getSize();
+	const calc = new AmountConverter(trade);
+	trade.tradeSizes = await calc.getSize();
 
-    // let logs = await tradeLogs(trade);
-    // logger.info(logs);
+	// let logs = await tradeLogs(trade);
+	// logger.info(logs);
 
-    // EDIT: now only calling getchGasPrice once per block index.ts.
-    // if (trade.gas.tested == false) {
-    //     let gas = await fetchGasPrice(trade);
-    //     if (gas.tested == false) {
-    //         console.log("Gas price not tested. Skipping trade.");
-    //         return trade;
-    //     }
-    //     trade.gas.tested = true;
-    // }
+	// EDIT: now only calling getchGasPrice once per block index.ts.
+	// if (trade.gas.tested == false) {
+	//     let gas = await fetchGasPrice(trade);
+	//     if (gas.tested == false) {
+	//         console.log("Gas price not tested. Skipping trade.");
+	//         return trade;
+	//     }
+	//     trade.gas.tested = true;
+	// }
 
-    const quotes = await getQuotes(trade);
+	const quotes = await getQuotes(trade);
 
-    const r = new PopulateRepays(trade, calc);
-    const repays = await r.getRepays();
-    trade.loanPool.repays = repays;
+	const r = new PopulateRepays(trade, calc);
+	const repays = await r.getRepays();
+	trade.loanPool.repays = repays;
 
-    const profitCalc = new ProfitCalculator(trade, calc, repays, quotes);
-    let p = {
-        multi: await profitCalc.getMultiProfit(),
-        single: await profitCalc.getSingleProfit(),
-        flashMulti: await profitCalc.getMultiFlashProfit(),
-        flashSingle: await profitCalc.getSingleFlashProfit(),
-    };
-    // if (
-    //     multi.flashProfit <= 0n &&
-    //     multi.singleProfit <= 0n &&
-    //     single.flashProfit <= 0n &&
-    //     single.singleProfit <= 0n
-    // ) {
-    //     trade.type = "filtered: 0 profit";
-    //     return trade;
-    // }
+	const profitCalc = new ProfitCalculator(trade, calc, repays, quotes);
+	let p = {
+		multi: await profitCalc.getMultiProfit(),
+		single: await profitCalc.getSingleProfit(),
+		flashMulti: await profitCalc.getMultiFlashProfit(),
+		flashSingle: await profitCalc.getSingleFlashProfit(),
+	};
+	// if (
+	//     multi.flashProfit <= 0n &&
+	//     multi.singleProfit <= 0n &&
+	//     single.flashProfit <= 0n &&
+	//     single.singleProfit <= 0n
+	// ) {
+	//     trade.type = "filtered: 0 profit";
+	//     return trade;
+	// }
 
-    let swapProfit = p.multi > p.single ? p.multi : p.single;
-    let flashProfit =
-        p.flashMulti > p.flashSingle ? p.flashMulti : p.flashSingle;
+	let swapProfit = p.multi > p.single ? p.multi : p.single;
+	let flashProfit =
+		p.flashMulti > p.flashSingle ? p.flashMulti : p.flashSingle;
 
-    let maxProfit = swapProfit > flashProfit ? swapProfit : flashProfit;
+	let maxProfit = swapProfit > flashProfit ? swapProfit : flashProfit;
 
-    trade.profits.tokenProfit = maxProfit;
+	trade.profits.tokenProfit = maxProfit;
 
-    if (maxProfit === p.multi) {
-        trade.type = "multi";
-    }
-    if (maxProfit === p.single) {
-        trade.type = "single";
-    }
-    if (maxProfit === p.flashMulti) {
-        trade.type = "flashMulti";
-    }
-    if (maxProfit === p.flashSingle) {
-        trade.type = "flashSingle";
-    }
+	if (maxProfit === p.multi) {
+		trade.type = "multi";
+	}
+	if (maxProfit === p.single) {
+		trade.type = "single";
+	}
+	if (maxProfit === p.flashMulti) {
+		trade.type = "flashMulti";
+	}
+	if (maxProfit === p.flashSingle) {
+		trade.type = "flashSingle";
+	}
 
-    trade.loanPool.amountRepay =
-        trade.type === "flashMulti"
-            ? repays.flashMulti
-            : trade.type === "flashSingle"
-            ? repays.flashSingle
-            : 0n; // Regular swaps have no repay as there is no loan.
+	trade.loanPool.amountRepay =
+		trade.type === "flashMulti"
+			? repays.flashMulti
+			: trade.type === "flashSingle"
+				? repays.flashSingle
+				: 0n; // Regular swaps have no repay as there is no loan.
 
-    if (trade.type === "single" || trade.type === "multi") {
-        trade.quotes = {
-            target: {
-                tokenInOut: quotes.target.tokenInOut,
-                tokenOutOut: quotes.target.tokenOutOut,
-            },
-            loanPool: {
-                tokenInOut: quotes.loanPool.tokenInOut,
-                tokenOutOut: quotes.loanPool.tokenOutOut,
-            },
-        };
-    }
-    if (trade.type.includes("flash"))
-        trade.quotes = {
-            target: {
-                tokenInOut: quotes.target.flashTokenInOut,
-                tokenOutOut: quotes.target.flashTokenOutOut,
-            },
-            loanPool: {
-                tokenInOut: quotes.loanPool.flashTokenInOut,
-                tokenOutOut: quotes.loanPool.flashTokenOutOut,
-            },
-        };
+	if (trade.type === "single" || trade.type === "multi") {
+		trade.quotes = {
+			target: {
+				tokenInOut: quotes.target.tokenInOut,
+				tokenOutOut: quotes.target.tokenOutOut,
+			},
+			loanPool: {
+				tokenInOut: quotes.loanPool.tokenInOut,
+				tokenOutOut: quotes.loanPool.tokenOutOut,
+			},
+		};
+	}
+	if (trade.type.includes("flash"))
+		trade.quotes = {
+			target: {
+				tokenInOut: quotes.target.flashTokenInOut,
+				tokenOutOut: quotes.target.flashTokenOutOut,
+			},
+			loanPool: {
+				tokenInOut: quotes.loanPool.flashTokenInOut,
+				tokenOutOut: quotes.loanPool.flashTokenOutOut,
+			},
+		};
 
-    trade.k = await getK(
-        trade.type,
-        trade.tradeSizes.loanPool.tradeSizeTokenIn.size,
-        trade.loanPool.reserveIn,
-        trade.loanPool.reserveOut,
-        calc,
-    );
+	trade.k = await getK(
+		trade.type,
+		trade.tradeSizes.loanPool.tradeSizeTokenIn.size,
+		trade.loanPool.reserveIn,
+		trade.loanPool.reserveOut,
+		calc,
+	);
 
-    if (trade.type === "single" || trade.type === "multi") {
-        trade.contract = swap;
-    }
-    if (trade.type === "flashMulti") {
-        trade.contract = flashMulti;
-    }
-    if (trade.type === "flashSingle") {
-        trade.contract = flashSingle;
-    }
+	if (trade.type === "single" || trade.type === "multi") {
+		trade.contract = swap;
+	}
+	if (trade.type === "flashMulti") {
+		trade.contract = flashMulti;
+	}
+	if (trade.type === "flashSingle") {
+		trade.contract = flashSingle;
+	}
 
-    trade.params = await params(trade);
+	trade.params = await params(trade);
 
-    return trade;
+	return trade;
 }
